98cb59e1edfebf5937f380d2ce975465420bf4ea
christopherhuii <christopher.kam.hui@gmail.com>
2015-01-27 22:21:44 -0800

    add solution of two stacks to make a queue. really understand how stacks and queues work now

---
+++ queue_two_stacks.rb
@@ -0,0 +1,104 @@
+# Implement a Queue class using two stacks. You may assume you have a stack class with the four methods listed below.
+
+# Stack and Queue Cheat Sheet
+
+# Queue: First in, first out
+
+# first
+# last
+# enqueue(item) #add
+# dequeue #remove
+# is_empty?
+
+# Stack: Last in, first out
+
+# peek
+# pop #remove
+# push(item) #add
+# is_empty?
+
+class Stack
+  def initialize
+    @list = []
+  end
+
+  def peek
+    @list[-1]
+  end
+
+  def remove
+    @list.pop
+  end
+
+  def add(item)
+    @list.push(item)
+  end
+
+  def is_empty?
+    @list == []
+  end
+end
+
+class Queue
+  def initialize
+    @stack1 = Stack.new
+    @stack2 = Stack.new
+  end
+
+  def first
+    move_to_stack_2
+    @stack2.peek
+  end
+
+  def last
+    move_to_stack_1
+    @stack1.peek
+  end
+
+  def enqueue(item)
+    move_to_stack_1
+    @stack1.add(item)
+  end
+
+  def dequeue
+    move_to_stack_2
+    @stack2.remove
+  end
+
+  def is_empty?
+    @stack1.is_empty? && @stack2.is_empty?
+  end
+
+  private
+
+  def move_to_stack_2
+    until @stack1.is_empty? do
+      @stack2.add(@stack1.remove)
+    end
+  end
+
+  def move_to_stack_1
+    until @stack2.is_empty? do
+      @stack1.add(@stack2.remove)
+    end
+  end
+end
+
+queue = Queue.new
+p queue.is_empty? == true
+queue.enqueue(1)
+queue.enqueue(2)
+p queue.first == 1
+p queue.last == 2
+queue.enqueue(3)
+p queue.dequeue == 1
+p queue.dequeue == 2
+queue.enqueue(4)
+p queue.first == 3
+p queue.last == 4
+p queue.is_empty? == false
+p queue.dequeue == 3
+p queue.dequeue == 4
+p queue.is_empty? == true
+
+
